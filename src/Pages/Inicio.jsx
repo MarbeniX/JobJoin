d69d1f91180894113f04eb087a70d7542172bb45
Berlin Ortega <marlitros200002@gmail.com>
2024-12-02 18:01:35 -0600

    si

--- src/Pages/Inicio.jsx
+++ src/Pages/Inicio.jsx
@@ -4,24 +4,23 @@ import { useNavigate } from "react-router-dom";
 
 export default function Header() {
     const navigate = useNavigate();
-    function Prueba1() {
-        console.log("Prueba1")
+    function Login() {
         navigate("/login");
     }
     return(
         <div className="inicio-main-div">
             <header className="inicio-header">
                 <h1 className="inicio-h1 general-h1">JobJoin</h1>
-                <Link to="/login" className="inicio-login-button inicio-header-button">Log in</Link>
-                <button onClick={Prueba1} className="inicio-signup-button inicio-header-button">Sign up</button>
+                <button onClick={Login} className="inicio-login-button inicio-header-button">Buscar</button>
+                <button onClick={Login} className="inicio-signup-button inicio-header-button">Unirse</button>
             </header>
 
             <div className="inicio-direccion-div">
                 <main className="inicio-direccion-main">
-                        <h2 className="inicio-direccion-main-h2">Servicios cerca de ti</h2>
+                        <h2 className="inicio-direccion-main-h2">La persona ideal para el trabajo que necesitas, <nav>en minutos</nav></h2>
                     <div className="inicio-direccion-div-ingreso">
                         <input type="text" placeholder="Ingresar dirección" className="inicio-direccion-input"/>
-                        <button>Buscar</button> 
+                        <button onClick={Login}>Buscar</button> 
                     </div>
                 </main>
             </div>

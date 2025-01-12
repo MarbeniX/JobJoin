ae52881022052c1522bac86bcc56998eedf465af
Ale <91446260+PeinSoR@users.noreply.github.com>
2025-01-11 22:22:31 -0600

    update headers
    redireccionamiento

--- src/Components/Headers/Header.jsx
+++ src/Components/Headers/Header.jsx
@@ -3,11 +3,12 @@ import JobJoinLogo from '../../Images/JobJoin.png'
 import { Link } from "react-router-dom";
 
 export default function Header() {
+
     return(
         <header className="inicio-header">
             <img src={JobJoinLogo} alt="JobJoin logo"/>
             <div className="header-links">
-                <Link to="/Buscar" className="inicio-login-button inicio-link">Buscar</Link>
+                <Link to="/buscar-sin-registro" className="inicio-login-button inicio-link">Buscar</Link>
                 <Link to="/login" className="inicio-signup-button inicio-link">Iniciar sesión</Link>
             </div>
         </header> 

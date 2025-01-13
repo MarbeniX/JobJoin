0a212a97b0b097931c9ad2732fa30adc2a7d4be1
Ale <91446260+PeinSoR@users.noreply.github.com>
2025-01-12 19:22:53 -0600

    Mensaje cuando te registras

--- src/Components/Headers/HeaderSesiónIniciada.jsx
+++ src/Components/Headers/HeaderSesiónIniciada.jsx
@@ -3,13 +3,22 @@ import Campanita from "../../Images/Campanita.png";
 import ProfilePhoto1 from "../../Images/ProfilePhoto1.png";
 import DownwardRow from "../../Images/DownwardRow.png";
 import "../../Css/Headers&Footer.css";
+import { useNavigate } from "react-router-dom";
 
 export default function HeaderSesiónIniciada(){
+    const navigate = useNavigate();
+
+    const handleBuscarConLogin = () => {
+        navigate('/busqueda-con-registro');
+    };
+
     return(
         <div className="header-sesion-iniciada">
             <img src={JobJoinLogo} alt="JobJoin Logo" className="header-sesion-iniciada__logo"/>
             <div className="headerSesionIniciada-links">
-                <button>Buscar</button>
+                <button
+                    onClick={handleBuscarConLogin}
+                >Buscar</button>
                 <button>Trabajo activo</button>
                 <img src={Campanita} alt="Campana de notificaciones" />
                 <div className="headerSesionIniciada-links-profile">

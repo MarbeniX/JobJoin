81ee109b33dfe9246ab1d21edd4f8e5fa071fb0e
Berlin Ortega <marlitros200002@gmail.com>
2025-01-12 21:22:14 -0600

    asdf

--- src/Components/TrabajoActivoStatus.jsx
+++ src/Components/TrabajoActivoStatus.jsx
@@ -1,9 +1,18 @@
 import FullProfilePicture from "../Images/FullProfilePicture.png";
+import RelojIcon from "../Images/RelojIcon.png";
 
 export default function TrabajoActivoStatus(){
     return(
         <div>
             <img src={FullProfilePicture} alt="FullProfilePicture" />
+            <div>
+                <div>
+                    <span><img src={RelojIcon} alt="RelojIcon" />En espera de respuesta</span>
+                    <p>Juan Pérez</p>
+                    <p>Plomería</p>
+                </div>
+                <button>Cancelar servicio</button>
+            </div>
         </div>
     )
 }
--- src/Pages/TrabajoActivo.jsx
+++ src/Pages/TrabajoActivo.jsx
@@ -1,11 +1,15 @@
 import Header from "../Components/Headers/HeaderSesiónIniciada";
 import Footer from "../Components/Footer"
+import TrabajoActivoStatus from "../Components/TrabajoActivoStatus";
+import Frame from "../Images/InicioDivisor.png"
 
 export default function TrabajoActivo(){
     return(
         <>
             <Header />
-            
+            <h1>Trabajo activo</h1>
+            <TrabajoActivoStatus />
+            <img src={Frame} alt="Frame" />
             <Footer />
         </>
     )

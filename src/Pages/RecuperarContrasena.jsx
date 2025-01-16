0802d403853da378eb9b8e9256aee5c7828d9293
Berlin Ortega <marlitros200002@gmail.com>
2025-01-16 01:18:00 -0600

    hola ale

--- src/Pages/RecuperarContrasena.jsx
+++ src/Pages/RecuperarContrasena.jsx
@@ -1,14 +1,14 @@
-import "../Css/RecuperarContrasena.css"
-import Header from "../Components/Headers/HeaderRecuperarContraseña"
-import React, { useState } from "react";
-import formulario from "../Images/formulario.png";
-import axios from "axios";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import Header from '../Components/Headers/Header.jsx';
+import formulario from '../Images/formulario.png';
+import '../Css/RecuperarContrasena.css';
 
-
-function RecuperarContrasena() {
-    const [email, setEmail] = useState("");
-    const [message, setMessage] = useState("");
+export default function RecuperarContrasena() {
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    const navigate = useNavigate();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -20,15 +20,24 @@ function RecuperarContrasena() {
         } catch (error) {
             setMessage(error.response.data.message);
         }
-    }
-    
+    };
+
+    useEffect(() => {
+        if (message === "Correo enviado correctamente") {
+            const timer = setTimeout(() => {
+                navigate("/recuperarContrasenaStep1"); // Cambia "/otra-pagina" por la ruta a la que deseas redirigir
+            }, 2000); // 2 segundos
+
+            return () => clearTimeout(timer); // Limpiar el temporizador si el componente se desmonta
+        }
+    }, [message, navigate]);
 
     return (
         <div className="recuperar-contraseña-div">
-            <Header/>
+            <Header />
             <main>
                 <div className="container">
-                    <img src={formulario} alt="Ilustración"/>
+                    <img src={formulario} alt="Ilustración" />
                     <div className="card">
                         <div className="logo">JobJoin</div>
                         <div className="title">Recupera tu cuenta</div>
@@ -39,12 +48,12 @@ function RecuperarContrasena() {
                         </div>
                         <form onSubmit={handleSubmit}>
                             <div className="form-group">
-                                <input type="email" 
-                                        placeholder="ejemplo@email.com" 
-                                        value={email}
-                                        onChange={(e) => setEmail(e.target.value)}
-                                        required
-                                    />
+                                <input type="email"
+                                    placeholder="ejemplo@email.com"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    required
+                                />
                             </div>
                             <div className="form-group">
                                 <Link type="submit" onClick={handleSubmit}>Enviar código</Link>
@@ -57,5 +66,3 @@ function RecuperarContrasena() {
         </div>
     );
 }
-
-export default RecuperarContrasena;
